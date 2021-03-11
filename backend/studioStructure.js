b242cd3e4d13bfde779b57b01316d193b16fac01
ordyboii <jake.ord345@gmail.com>
2021-03-11 00:06:49 +0000

    Fixed a bug

--- backend/studioStructure.js
+++ backend/studioStructure.js
@@ -1,5 +1,3 @@
 import S from "@sanity/desk-tool/structure-builder";
 
-export default () => S.list().title("content").items(
-    S.documentTypeListItem();
-);
+export default () => S.list().title("content").items(S.documentTypeListItem());
