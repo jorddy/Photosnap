2684b982fd09d9b0c7b0e4a041ccc7b41d6dbf1e
ordyboii <jake.ord345@gmail.com>
2021-03-11 00:06:18 +0000

    Initial commit

--- backend/studioStructure.js
+++ backend/studioStructure.js
@@ -1,3 +1,5 @@
 import S from "@sanity/desk-tool/structure-builder";
 
-export default () => S.list().title("content").items();
+export default () => S.list().title("content").items(
+    S.documentTypeListItem();
+);
